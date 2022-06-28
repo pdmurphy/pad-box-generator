d2256c0e50b8b42401f258462b401a882b667cfa
Purtle <pdmurphy28@gmail.com>
2022-06-28 18:08:21 -0400

    First commit

---
+++ source/PadBoxImageGenerator.py
@@ -0,0 +1,7 @@
+#using list of IDs 
+#open each image and categorize into 6 (5+colorless) separate array lists based on color
+
+#combine the 6 arrays back into 1 list of IDs that will now be organized by color
+#returning that combined/organized list
+
+#run image generator that takes list of IDs and creates combined image of them all
